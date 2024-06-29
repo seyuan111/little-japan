5a387269b5e010432cd812b9fe25b9d414203f3b
Sean Yuan <ysean54@yahoo.com>
2024-06-29 17:34:02 -0400

    little-japan project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import NavBar from './components/NavBar'
+
+function App() {
+
+  return (
+    <div className="app">
+      <NavBar />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../seperateCSS/NavBar.css'
+
+const NavBar = () => {
+  return (
+    <div className="navbar">
+      
+    </div>
+  )
+}
+
+export default NavBar
