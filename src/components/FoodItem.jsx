eeb5c18ba31905b31b68d0436eedc9795434e378
Sean Yuan <ysean54@yahoo.com>
2024-07-11 22:11:05 -0400

    styled the plus sign

--- src/components/FoodItem.jsx
+++ src/components/FoodItem.jsx
@@ -1,12 +1,25 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { FaPlus } from "react-icons/fa";
+import { IoIosRemove } from "react-icons/io";
+import { IoIosAdd } from "react-icons/io";
 import '../seperateCSS/FoodItem.css'
 
 const FoodItem = ({id, name, price, description, image}) => {
-    console.log(price)
+    const [countItem, setCountItem] = useState(0)
+
   return (
     <div className="food-item">
         <div className="food-item-img-container">
             <img className="food-item-img" src={image}></img>
+            {
+                !countItem ? <FaPlus className="add" onClick={() => setCountItem(prev=>prev+1)} /> :
+                
+                <div className="food-item-counter">
+                    <IoIosAdd onClick={() => setCountItem(prev=> prev+1)}/>
+                    <p>{countItem}</p>
+                    <IoIosRemove onClick={() => setCountItem(prev=> prev-1)}/>
+                </div>
+            }
         </div>
         <div className="food-item-info">
             <div className="food-item-name-ratings">
