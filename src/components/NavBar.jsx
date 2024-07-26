c9a499cefd61d64ef4a58ff3ea9d92504c110c1a
Sean Yuan <ysean54@yahoo.com>
2024-07-25 22:38:31 -0400

    styled the signup login and added link to cart

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 import '../seperateCSS/NavBar.css';
 import { FaSearch, FaShoppingCart, FaBars, FaTimes } from "react-icons/fa";
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
-const NavBar = ({setShowLogin}) => {
+const NavBar = ({ setShowLogin }) => {
   const [menu, setMenu] = useState("home");
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
@@ -11,24 +11,29 @@ const NavBar = ({setShowLogin}) => {
     setIsMobileMenuOpen(!isMobileMenuOpen);
   };
 
+  const handleMenuClick = (menuItem) => {
+    setMenu(menuItem);
+    setIsMobileMenuOpen(false); // Close the mobile menu
+  };
+
   return (
     <div className="navbar">
-      <img 
+      <Link to="/"><img 
         src="https://c8.alamy.com/comp/2J2TJ03/sushi-logo-in-a-bowl-chopsticks-swoosh-bowl-oriental-japan-cuisine-japanese-sushi-seafood-logo-design-inspiration-2J2TJ03.jpg" 
         alt="Logo" 
         className="japaneseLogo"
-      />
+      /></Link>
       <ul className={`menu ${isMobileMenuOpen ? 'active' : ''}`}>
-        <Link onClick={() => setMenu("home")} className={menu === "home" ? "active" : ""}>Home</Link>
-        <a href="#explore-menu" onClick={() => setMenu("menu")} className={menu === "menu" ? "active" : ""}>Categories</a>
-        <a href="#food-display" onClick={() => setMenu("mobile")} className={menu === "mobile" ? "active" : ""}>Menu</a>
-        <a href="#footer" onClick={() => setMenu("contact")} className={menu === "contact" ? "active" : ""}>Contact</a>
+        <Link to="/" onClick={() => handleMenuClick("home")} className={menu === "home" ? "active" : ""}>Home</Link>
+        <a href="#explore-menu" onClick={() => handleMenuClick("menu")} className={menu === "menu" ? "active" : ""}>Categories</a>
+        <a href="#food-display" onClick={() => handleMenuClick("mobile")} className={menu === "mobile" ? "active" : ""}>Menu</a>
+        <a href="#footer" onClick={() => handleMenuClick("contact")} className={menu === "contact" ? "active" : ""}>Contact</a>
       </ul>
 
       <div className="right">
         <FaSearch className="search" />
         <div className="search-icon-navbar">
-          <FaShoppingCart className="shopping-cart" />
+          <Link to="/cart"><FaShoppingCart className="shopping-cart" /></Link>
           <div className="dot"></div>
         </div>
         <button onClick={() => setShowLogin(true)}>Sign In</button>
